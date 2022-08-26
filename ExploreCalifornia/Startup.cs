38b651ea3490136eb125f3611506a763480b86e5
Reza Abbaspourshirazifard <shirazi.reza.sh@gmail.com>
2022-08-25 17:27:51 -0700

    added app insight

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -60,6 +60,7 @@ namespace ExploreCalifornia
 
             services.AddScoped<SignInManager<User>>();
             services.Configure<ConnectionStrings>(Configuration.GetSection("ConnectionStrings"));
+            services.AddApplicationInsightsTelemetry(Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
